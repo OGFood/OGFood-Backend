2d5d77e08a89f304eae29f1a63ee42300f175039
CertifiedIdiot <leon.larsson@gmail.com>
2022-05-06 19:39:51 +0200

    ApiGetter Project Demo

---
+++ ApiGetter/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ApiGetter;
+
+Console.WriteLine(await ApiGet.ApiRequest($"https://pokeapi.co/api/v2/pokemon?offset=20&limit=20"));
