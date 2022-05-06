825888ea2954beda0903f6c1d7fda0c0e18cc260
Thomas Thorin <thomas.thorin@gmail.com>
2022-05-06 14:45:11 +0200

    DBAccess first draft

---
+++ DbAccess/Factory.cs
@@ -0,0 +1,10 @@
+﻿namespace DbAccess
+{
+    using Interfaces;
+    using Helpers;
+
+    public static class Factory
+    {
+        public static IConnectionStringHelper GetConnectionStringHelper() => ConnectionStringHelper.Instance;
+    }
+}
