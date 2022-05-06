a985ed5bce615daab3d05bafe7f45d679e22deb3
Thomas Thorin <thomas.thorin@gmail.com>
2022-05-06 15:16:26 +0200

    update ingredient crud interface to include nameBeginsWith

--- DbAccess/Interfaces/IIngredientCrud.cs
+++ DbAccess/Interfaces/IIngredientCrud.cs
@@ -6,5 +6,6 @@
     {
         public Task<Ingredient> GetIngredientByName(string name);
         public Task<Ingredient> GetIngredientById(string id);
+        public Task<List<Ingredient>> GetIngredientsByNameBeginsWith(string searchString);
     }
 }
