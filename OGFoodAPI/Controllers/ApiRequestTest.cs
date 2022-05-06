6af1ee7a37f893ad20b8a91e8923bdcf77b56450
Marcus-Kanon <94marsun@gafe.molndal.se>
2022-05-06 19:12:13 +0200

    fgfgh

---
+++ OGFoodAPI/Controllers/ApiRequestTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OGFoodAPI.ApiCaller;
+
+namespace OGFoodAPI.Controllers
+{
+    public class ApiRequestTest : ControllerBase
+    {
+        [Route("test")]
+        public string Index()
+        {
+            SelectStrat callApi = new SelectStrat();
+            callApi.Go();
+
+            return "test";
+        }
+    }
+}
