61d839b287883cc5b63a08520afb0a89cb92ea5a
Thomas Thorin <thomas.thorin@gmail.com>
2022-05-06 15:13:07 +0200

    added IngredientCrud

--- DbAccess/Factory.cs
+++ DbAccess/Factory.cs
@@ -2,9 +2,13 @@
 {
     using Interfaces;
     using Helpers;
+    using Database;
 
     public static class Factory
     {
         public static IConnectionStringHelper GetConnectionStringHelper() => ConnectionStringHelper.Instance;
+
+        internal static MongoDbAccess GetDbAccess() => new(GetConnectionStringHelper());
+        public static IIngredientCrud GetIngredientCrud() => new MongoIngredientCrud(GetDbAccess());
     }
 }
---
+++ DbAccess/Interfaces/IIngredientCrud.cs
@@ -0,0 +1,10 @@
+﻿namespace DbAccess.Interfaces
+{
+    using DbAccess.Models;
+
+    public interface IIngredientCrud
+    {
+        public Task<Ingredient> GetIngredientByName(string name);
+        public Task<Ingredient> GetIngredientById(string id);
+    }
+}
