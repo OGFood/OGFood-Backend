952a2d0b6642ddf933804996a10ef8ac604d15e7
Thomas Thorin <89643077+tthorin@users.noreply.github.com>
2022-05-27 01:30:39 +0200

    Update Program.cs

--- OGFoodAPI/Program.cs
+++ OGFoodAPI/Program.cs
@@ -12,7 +12,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("Policy1",
         policy =>
         {
-            policy.WithOrigins("http://localhost:3000");
+            policy.WithOrigins("*");
         });
 });
 
