6a54a7ac79db24da4567d875dec2643f55a55eb0
CertifiedIdiot <leon.larsson@gmail.com>
2022-05-27 11:52:13 +0200

    updates ppl

--- DbAccess/Database/MongoUserCrud.cs
+++ DbAccess/Database/MongoUserCrud.cs
@@ -23,27 +23,25 @@ namespace DbAccess.Database
         }
 
         // Create
-        public async Task<bool> CreateUser(string name, string mail, string password)
+        public async Task<bool> CreateUser(User user)
         {
-            string salt;
-
             // Valid inputs?
-            if (string.IsNullOrEmpty(name) ||
-                !mailHelper.IsMailValid(mail) ||
-                password.Length < 12)
+            if (string.IsNullOrEmpty(user.Name) ||
+                !mailHelper.IsMailValid(user.Mail) ||
+                user.Password.Length < 12)
             {
                 return false;
             }
 
             // Name/Mail taken?
-            if (IsNameOrMailTaken(name, mail).Result)
+            if (await IsNameOrMailTaken(user.Name, user.Name))
             {
                 return false;
             }
 
             // Prepare user to insert
-            salt = pwdHelper.GetSalt();
-            User user = new(name, mail, salt, pwdHelper.GetSaltedHash(password, salt));
+            user.Salt = pwdHelper.GetSalt();
+            user.Password = pwdHelper.GetSaltedHash(user.Password, user.Salt);
 
             // Insert user
             try
@@ -59,26 +57,9 @@ namespace DbAccess.Database
         }
 
         // Read
-
-        public async Task<bool> DeleteUser(string name, string password)
+        private async Task<User> GetUserById(string id)
         {
-            var user = GetUserById(UserNameToId(name));
-
-            if(pwdHelper.IsPwdValid(password, user.Salt, user.Password))
-            try
-            {
-                await Users.DeleteOneAsync(u => u.Name == name);
-                return true;
-            }
-            catch
-            {
-                return false;
-            }
-        }
-
-        public async Task<User> GetUserById(string id)
-        {
-            return await Users.FindAsync(u => u.Id == id).Current.FirstOrDefault();
+            return (await Users.FindAsync(u => u.Id == id)).Current.FirstOrDefault()!;
         }
 
         public Task<User> GetUserByMail(string mail, string password)
@@ -88,13 +69,11 @@ namespace DbAccess.Database
 
         private async Task<string> UserNameToId(string name)
         {
-            var user = await Users.FindAsync(u => u.Name == name);
-            return user.Current.FirstOrDefault().Name;
+            return (await Users.FindAsync(u => u.Name == name)).Current.FirstOrDefault()!.Id;
         }
-
-        public async Task<User>GetUserByName()
+        public async Task<User> GetUserByName(string name, string password)
         {
-
+            return (await Users.FindAsync(u => u.Name == name)).Current.FirstOrDefault()!;
         }
 
         public async Task<bool> IsNameOrMailTaken(string name = "", string mail = "")
@@ -105,17 +84,38 @@ namespace DbAccess.Database
             return userName == null || userMail == null;
         }
 
+        // Update
         public Task UpdateUser(string name, string oldPassword, string newUsername = "", string newPassword = "")
         {
             throw new NotImplementedException();
         }
 
-        public Task UpdateUserIngredients(string name, string password)
+        public Task AddUserIngredient(string name, string password, List<Ingredient> ingredients)
         {
             throw new NotImplementedException();
         }
+        // Delete
+        public async Task<bool> DeleteUser(string name, string password)
+        {
+            var user = await GetUserById(await UserNameToId(name));
 
-        public Task UpdateUserWithMail(string mail, string newUsername = "", string newPassword = "")
+            if (pwdHelper.IsPwdValid(password, user.Salt, user.Password))
+            {
+                try
+                {
+                    await Users.DeleteOneAsync(u => u.Name == name);
+                    return true;
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+
+            return false;
+        }
+
+        Task<bool> IUserCrud.AddUserIngredient(string name, string password, List<Ingredient> ingredients)
         {
             throw new NotImplementedException();
         }
@@ -125,5 +125,9 @@ namespace DbAccess.Database
             throw new NotImplementedException();
         }
 
+        public Task<bool> RemoveUserIngredient(string name, string password, List<Ingredient> ingredients)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
