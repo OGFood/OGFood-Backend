9673b47a27969c32d3ab17e0818f412befcbaa76
Thomas Thorin <thomas.thorin@gmail.com>
2022-06-01 01:09:12 +0200

    removed builder.Services.AddControllers().AddNewtonsoftJson(); from program.cs

--- OGFoodAPI/Program.cs
+++ OGFoodAPI/Program.cs
@@ -8,7 +8,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers().AddNewtonsoftJson();
+
+//builder.Services.AddControllers().AddNewtonsoftJson();
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
