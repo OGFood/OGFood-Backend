c0c54ddf673dfbc2049fb97d249461f280fa93b2
Thomas Thorin <thomas.thorin@gmail.com>
2022-06-01 09:52:34 +0200

    removed interfaces from poco classes

--- SharedInterfaces/Models/User.cs
+++ SharedInterfaces/Models/User.cs
@@ -1,21 +1,11 @@
 ﻿using MongoDB.Bson.Serialization.Attributes;
 using MongoDB.Bson.Serialization.Serializers;
-using Newtonsoft.Json;
 using SharedInterfaces.Interfaces;
-using System.Collections.Generic;
 
 namespace SharedInterfaces.Models
 {
-    public class User : IUser
+    public class User //: IUser
     {
-        public User(string name, string mail, string salt, string password)
-        {
-            Name = name;
-            Mail = mail;
-            Salt = salt;
-            Password = password;
-        }
-
         [BsonId]
         [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
         public string Id { get; set; } = "";
